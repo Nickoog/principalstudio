76dd4072b882abcaf566b69f4e00dfbce60af300
Nicolas Hoog <nicolas.hoog@gmail.com>
2019-04-13 12:30:06 -0400

    setup css file fonts images

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.render('index');
 });
 
 app.listen(process.env.PORT || 4000, function(){
